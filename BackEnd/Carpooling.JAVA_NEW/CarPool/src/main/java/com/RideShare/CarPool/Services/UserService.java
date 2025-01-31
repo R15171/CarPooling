feabdd54bc97f89349e2d1d9bebf9c031b9b7247
Rinkesh V Mailapur <Rinkeshvm@gmail.com>
2025-01-31 15:26:19 +0530

    Login, Registartion, Update Profile , Email Service
    Using SpringBoot Rest Api is created for Login , Registartion , Update Profile and Email Notification (after successful registartion) is Developed

---
+++ BackEnd/Carpooling.JAVA_NEW/CarPool/src/main/java/com/RideShare/CarPool/Services/UserService.java
@@ -0,0 +1,125 @@
+//package com.RideShare.CarPool.Services;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.RideShare.CarPool.Controllers.UserController;
+//import com.RideShare.CarPool.Entities.User;
+//import com.RideShare.CarPool.Repositories.UserRepository;
+//
+//@Service
+//public class UserService {
+//
+//	@Autowired 
+//	UserRepository userRepository;
+//	
+//	public List<User> getAll(){
+//		return userRepository.findAll();
+//	}//getAll()
+//	
+//	
+//	public User getUserByContact(String contactno) {
+//		System.out.println("ser");
+//		return userRepository.findUserByContact(contactno);
+//	}//getUserByContact()
+//	
+//	
+//	
+//	
+//	public User userById(int id) {
+//		return userRepository.findById(id).get();
+//	}//findbyid for login verification
+//	
+//	
+//	
+//	public User userRegister(User user) {
+//		return userRepository.save(user);
+//	}//UserRegistration
+//	
+//	
+//	
+//	
+//}//UserService
+
+
+
+
+
+
+
+
+
+
+
+package com.RideShare.CarPool.Services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.RideShare.CarPool.Entities.User;
+import com.RideShare.CarPool.Repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired 
+    private UserRepository userRepository;
+
+    @Autowired
+    private EmailService emailService; // Inject EmailService
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public User findUserByContact(String contactno) {
+        return userRepository.findUserByContact(contactno);
+    }//find user by contactno
+
+    public User findUserById(int id) {
+        return userRepository.findById(id).orElse(null);
+    }//find user by id
+
+    public User userRegister(User user) {
+        User savedUser = userRepository.save(user);
+
+        // Send welcome email
+        String subject = "Welcome to RideShare!";
+        String body = "Hello " + user.getName() + ",\n\nWelcome to Carpool! Your account has been successfully created.\n\nEnjoy your rides!\n\nBest Regards,\nCarpooling Team";
+        emailService.sendEmail(user.getEmail(), subject, body);
+
+        return savedUser;
+    }//user Register
+
+
+
+    
+    public boolean updateProfile(User profile) {
+        Optional<User> userRegistered = userRepository.findById(profile.getUid());
+
+        if (!userRegistered.isPresent()) {
+            return false; // User not found
+        }
+
+        User old = userRegistered.get();  // Extract the user from Optional
+
+        old.setName(profile.getName());
+        old.setEmail(profile.getEmail());
+        old.setContactno(profile.getContactno());
+        old.setAddress(profile.getAddress());
+        old.setDob(profile.getDob());
+        old.setPassword(profile.getPassword());
+        old.setGender(profile.getGender());
+
+        userRepository.save(old);  // Save updated user
+        return true; // Successful update
+    }
+    
+    
+    
+}//service class
+
